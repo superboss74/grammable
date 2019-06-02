9b346d7d6e9fbe274e714c174177e4010d4b6627
Hugo Mejia <superboss74@protonmail.com>
2019-06-02 21:09:13 +0000

    Adding support for Creating  a Gram

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,8 +1,22 @@
+# This Controllers allows for Grams actions
+# in this project
 class GramsController < ApplicationController
   def new
     @gram = Gram.new
   end
 
   def index
+    # placeholder to build the index page
+  end
+
+  def create
+    @gram = Gram.create(gram_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
   end
 end
