3b68bdd74972efb801fdfa49a2eef75b4a9eb32d
Hugo Mejia <superboss74@protonmail.com>
2019-06-02 19:49:31 +0000

    Adding Gram model to DB and Form view placeholder for it

--- spec/helpers/grams_helper_spec.rb
+++ spec/helpers/grams_helper_spec.rb
@@ -10,11 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe GramsController, type: :controller do
-  describe "grams#index action" do
-    it "should successfully show the page" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
+RSpec.describe GramsHelper, type: :helper do
+
 end
